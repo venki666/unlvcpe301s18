133c2e72b048477cc660fedf0df7e6cc7ee13b2f
Venki <4.8032x10e10@gmail.com>
2018-02-07 23:27:10 -0800

    First Class Demo

---
+++ Firstasm_application/Firstasm_application/main.asm
@@ -0,0 +1,23 @@
+.cseg
+start:
+	;; Load arbitrary immediates into registers 16:20
+  ldi		r16,	59
+	ldi		r17,	29
+	ldi		r18,	39
+	ldi		r19,	59
+	ldi		r20,	99
+
+	sbi		DDRB,	2		;; Set port B pin 2 as output
+	out		PORTB,	r0		;; Set all outputs to 0
+
+	;; Add the integers
+	add		r16,	r17
+	add		r16,	r18
+	add		r16,	r19
+	add		r16,	r20
+
+	brcc	End				;; Check if carry flag set
+	ldi		r17,	4
+	out		PORTB,	r17		;; Set output high at pin2 on port B
+End:
+    rjmp	End
